37015d81038377c4bd317c6cf1086b694642c4f9
Christian Torres <chtorrez@gmail.com>
2019-07-05 14:04:55 -0600

    Starting navigation and showing loading.

---
+++ src/components/Loader.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="loader">
+    <div>
+      <img src="/images/preloader.gif" />
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+
+@Component
+export default class Loader extends Vue {
+}
+</script>
--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,16 @@
-import 'bootstrap/dist/css/bootstrap.css'
-import 'bootstrap-vue/dist/bootstrap-vue.css'
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap-vue/dist/bootstrap-vue.css';
 
 import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
 import store from './store';
 import BootstrapVue from 'bootstrap-vue';
+import Loader from './components/Loader.vue';
 
 Vue.config.productionTip = false;
 Vue.use(BootstrapVue);
+Vue.component('Loader', Loader);
 
 new Vue({
   router,
